5c730e17a5f09130affa7f78bb542d5bef60efe4
SkrilpGit <jessica.smith@futuregen.net.au>
2026-03-07 18:42:19 +1100

    added da plan

--- calc.c
+++ calc.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
-
+/*
+ * Make an iterative calculator that recognises numbers and splits them by operations
+ * we want:
+ * number and operator recognising
+ * iterative processing that is resistant to non typical inputs
+ * support for ()
+ * quitting on "q" "quit" "exit" "kill" and any other thingys
+ * support + - * / % ^ log
+ * more to come!
+ */
 int main(){
     printf("Hello Calc\n");
     return 1;
